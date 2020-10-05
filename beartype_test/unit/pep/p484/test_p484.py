ad503645138721878636226139bdc37754c7f06e
leycec <leycec@gmail.com>
2020-10-05 17:02:27 -0400

    Resolve #5 x 2.
    This commit further reduces the likelihood of similar issues to #5 by
    augmenting the core `test_p484()` unit test with logic exercising
    memoization of different callables annotated by the same PEP-compliant
    type hints.
    
    ## Tests Improved
    
    * `test_p484()`, an existing unit test augmented to exercise this edge
      case, reducing the likelihood of regressions.
    
    (Circumspect electricity evolves circumlocutionary alacrity!)

--- beartype_test/unit/pep/p484/test_p484.py
+++ beartype_test/unit/pep/p484/test_p484.py
@@ -36,8 +36,15 @@ def test_p484() -> None:
     passed non-variadic positional and/or keyword parameters annotated with
     `PEP 484`_-compliant type hints.
 
+    This test additionally attempts to avoid similar issues to a `prior issue
+    <issue #5_>`__ of this decorator induced by repeated
+    :func:`beartype.beartype` decorations of different callables annotated by
+    one or more of the same PEP-compliant type hints.
+
     .. _PEP 484:
        https://www.python.org/dev/peps/pep-0484
+    .. _issue #5:
+       https://github.com/beartype/beartype/issues/5
     '''
 
     # Defer heavyweight imports.
@@ -55,84 +62,92 @@ def test_p484() -> None:
     # argumentless "typing" attributes or not.
     PEP_HINT_ALL_TO_META = ChainMap(PEP_HINT_TO_META, PEP_HINT_NONATTR_TO_META)
 
+    # Tuple of two arbitrary values used to trivially iterate twice below.
+    RANGE_2 = (None, None)
+
     # For each predefined PEP-compliant type hint and associated metadata...
     for pep_hint, pep_hint_meta in PEP_HINT_ALL_TO_META.items():
-        # If this hint is currently unsupported, continue to the next.
-        if not pep_hint_meta.is_supported:
-            continue
-        # Else, this hint is currently supported.
+        # Repeat the following logic twice. Why? To exercise memoization across
+        # repeated @beartype decorations on different callables annotated by
+        # the same PEP hints.
+        for _ in RANGE_2:
+            # If this hint is currently unsupported, continue to the next.
+            if not pep_hint_meta.is_supported:
+                continue
+            # Else, this hint is currently supported.
 
-        # Dynamically define a callable both accepting a single parameter and
-        # returning a value annotated by this hint whose implementation
-        # trivially reduces to the identity function.
-        @beartype
-        def pep_hinted(pep_hinted_param: pep_hint) -> pep_hint:
-            return pep_hinted_param
+            # Dynamically define a callable both accepting a single parameter
+            # and returning a value annotated by this hint whose implementation
+            # trivially reduces to the identity function.
+            @beartype
+            def pep_hinted(pep_hinted_param: pep_hint) -> pep_hint:
+                return pep_hinted_param
 
-        # Type of exception raised by this wrapper on type-check failures.; else, this
-        # wrapper
-        # Note that these hints all .
-        exception_cls = (
-            # If this hint is uniquely identified by an argumentless "typing"
-            # attribute, this wrapper raises PEP-compliant exceptions.
-            BeartypeCallCheckPepException
-            if pep_hint_meta.typing_attr is not None else
-            # Else, this hint reduces to a builtin type and is thus detected as
-            # a PEP-noncompliant type hint. In this case, this wrapper raises
-            # PEP-noncompliant exceptions.
-            BeartypeCallCheckNonPepException
-        )
+            # Type of exception raised by this wrapper on type-check failures.
+            exception_cls = (
+                # If this hint is uniquely identified by an argumentless
+                # "typing" attribute, this wrapper raises PEP-compliant
+                # exceptions.
+                BeartypeCallCheckPepException
+                if pep_hint_meta.typing_attr is not None else
+                # Else, this hint reduces to a builtin type and is thus
+                # detected as a PEP-noncompliant type hint. In this case, this
+                # wrapper raises PEP-noncompliant exceptions.
+                BeartypeCallCheckNonPepException
+            )
 
-        # For each object satisfying this hint...
-        for pith_satisfied in pep_hint_meta.piths_satisfied:
-            # Assert this wrapper function successfully type-checks this
-            # object against this hint *WITHOUT* modifying this object.
-            # print('PEP-testing {!r} against {!r}...'.format(pep_hint, pith_satisfied))
-            assert pep_hinted(pith_satisfied) is pith_satisfied
+            # For each object satisfying this hint...
+            for pith_satisfied in pep_hint_meta.piths_satisfied:
+                # Assert this wrapper function successfully type-checks this
+                # object against this hint *WITHOUT* modifying this object.
+                # print('PEP-testing {!r} against {!r}...'.format(pep_hint, pith_satisfied))
+                assert pep_hinted(pith_satisfied) is pith_satisfied
 
-        # For each object *NOT* satisfying this hint...
-        for pith_unsatisfied_meta in pep_hint_meta.piths_unsatisfied_meta:
-            # Assert that iterables of uncompiled regular expression expected
-            # to match and *NOT* match this message are *NOT* strings, as
-            # commonly occurs when accidentally omitting a trailing comma in
-            # tuples containing only one string: e.g.,
-            # * "('This is a tuple, yo.',)" is a 1-tuple containing one string.
-            # * "('This is a string, bro.')" is a string *NOT* contained in a
-            #   1-tuple.
-            assert not isinstance(
-                pith_unsatisfied_meta.exception_str_match_regexes, str)
-            assert not isinstance(
-                pith_unsatisfied_meta.exception_str_not_match_regexes, str)
+            # For each object *NOT* satisfying this hint...
+            for pith_unsatisfied_meta in pep_hint_meta.piths_unsatisfied_meta:
+                # Assert that iterables of uncompiled regular expression
+                # expected to match and *NOT* match this message are *NOT*
+                # strings, as commonly occurs when accidentally omitting a
+                # trailing comma in tuples containing only one string: e.g.,
+                # * "('This is a tuple, yo.',)" is a 1-tuple containing one
+                #   string.
+                # * "('This is a string, bro.')" is a string *NOT* contained in
+                #   a 1-tuple.
+                assert not isinstance(
+                    pith_unsatisfied_meta.exception_str_match_regexes, str)
+                assert not isinstance(
+                    pith_unsatisfied_meta.exception_str_not_match_regexes, str)
 
-            # Assert this wrapper function raises the expected exception when
-            # type-checking this object against this hint.
-            with raises_uncached(exception_cls) as exception_info:
-                pep_hinted(pith_unsatisfied_meta.pith)
+                # Assert this wrapper function raises the expected exception
+                # when type-checking this object against this hint.
+                with raises_uncached(exception_cls) as exception_info:
+                    pep_hinted(pith_unsatisfied_meta.pith)
 
-            # Exception message raised by this wrapper function.
-            exception_str = str(exception_info.value)
-            # print('exception message: {}'.format(exception_str))
+                # Exception message raised by this wrapper function.
+                exception_str = str(exception_info.value)
+                # print('exception message: {}'.format(exception_str))
 
-            # For each uncompiled regular expression expected to match this
-            # message, assert this expression actually does so.
-            #
-            # Note that the re.search() rather than re.match() function is
-            # called. The latter is rooted at the start of the string and thus
-            # *ONLY* matches prefixes, while the former is *NOT* rooted at any
-            # string position and thus matches arbitrary substrings as desired.
-            for exception_str_match_regex in (
-                pith_unsatisfied_meta.exception_str_match_regexes):
-                assert search(
-                    exception_str_match_regex, exception_str) is not None
+                # For each uncompiled regular expression expected to match this
+                # message, assert this expression actually does so.
+                #
+                # Note that the re.search() rather than re.match() function is
+                # called. The latter is rooted at the start of the string and
+                # thus *ONLY* matches prefixes, while the former is *NOT*
+                # rooted at any string position and thus matches arbitrary
+                # substrings as desired.
+                for exception_str_match_regex in (
+                    pith_unsatisfied_meta.exception_str_match_regexes):
+                    assert search(
+                        exception_str_match_regex, exception_str) is not None
 
-            # For each uncompiled regular expression expected to *NOT* match
-            # this message, assert this expression actually does so.
-            for exception_str_not_match_regex in (
-                pith_unsatisfied_meta.exception_str_not_match_regexes):
-                assert search(
-                    exception_str_not_match_regex, exception_str) is None
+                # For each uncompiled regular expression expected to *NOT*
+                # match this message, assert this expression actually does so.
+                for exception_str_not_match_regex in (
+                    pith_unsatisfied_meta.exception_str_not_match_regexes):
+                    assert search(
+                        exception_str_not_match_regex, exception_str) is None
 
-        # assert False is True
+            # assert False is True
 
 # ....................{ TESTS ~ issue                     }....................
 def test_p484_sequence_standard_cached() -> None:
@@ -141,6 +156,10 @@ def test_p484_sequence_standard_cached() -> None:
     decorator with respect to metadata describing **PEP-compliant standard
     sequence hints** (e.g., :attr:`typing.List`) cached via memoization across
     calls to that decorator has been resolved and *not* regressed.
+
+    Note that the more general-purpose :func:`test_p484` test *should* already
+    exercise this issue, but that this issue was sufficiently dire to warrant
+    special-purposed testing exercising this exact issue.
 
     .. _issue #5:
        https://github.com/beartype/beartype/issues/5
