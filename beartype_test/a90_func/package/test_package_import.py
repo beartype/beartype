2df05b76f656eb5de98f632f62c5c1f3c57f5b39
leycec <leycec@gmail.com>
2021-12-02 03:10:57 -0400

    Subpackage import deferral x 3.
    This commit is the next in a commit chain guaranteeing optional first-
    and third-party subpackages to *not* be unconditionally imported by the
    initial ``from beartype import beartype`` statement. Specifically, this
    commit finalizes our new `test_import_isolation()` functional test
    guaranteeing this to be the case. Since the `@beartype` decorator
    currently violates this constraint, tests are now superficially broken
    and will (probably) remain so until the thrilling conclusion of this
    commit cycle. GitHub Best Sellers: *ain't nobody buyin'.* (*Energetic emetic demiurge!*)

--- beartype_test/a90_func/package/test_package_import.py
+++ beartype_test/a90_func/package/test_package_import.py
@@ -30,6 +30,34 @@ def test_package_import_isolation() -> None:
     # Defer heavyweight imports.
     from beartype_test.util.cmd.pytcmdrun import (
         run_python_forward_stderr_return_stdout)
+    from re import (
+        compile as re_compile,
+        search as re_search,
+    )
+
+    #FIXME: *FRAGILE.* Manually hardcoding module names here invites
+    #desynchronization, particularly with optional third-party dependencies.
+    #Instead, the names of optional third-party packages should be dynamically
+    #constructed from the contents of the "beartype.meta" submodule.
+
+    # Tuple of uncompiled regular expressions, each matching the
+    # fully-qualified name of *ANY* heavyweight (sub)module or package whose
+    # importation violates our self-imposed constraint of fast importation of
+    # our core @beartype.beartype decorator.
+    _HEAVY_MODULE_NAME_RAW_REGEXES = (
+        r'beartype\.cave',
+        r'beartype\.vale',
+        r'numpy',
+        r'typing_extensions',
+    )
+
+    # Uncompiled regular expressions synthesized from this tuple.
+    _HEAVY_MODULE_NAME_RAW_REGEX = '|'.join(_HEAVY_MODULE_NAME_RAW_REGEXES)
+
+    # Compiled regular expression synthesized from this tuple.
+    _HEAVY_MODULE_NAME_REGEX = re_compile(
+        fr'^({_HEAVY_MODULE_NAME_RAW_REGEX})\b')
+    # print(f'_HEAVY_MODULE_NAME_REGEX: {_HEAVY_MODULE_NAME_REGEX}')
 
     # Python code printing all imports (i.e., the contents of the standard
     # "sys.modules" list) *AFTER* importing the @beartype decorator.
@@ -51,8 +79,26 @@ print('\\n'.join(module_name for module_name in modules.keys()))
     # subprocess failure while both forwarding all standard error output by
     # this subprocess to the standard error file handle of the active Python
     # process *AND* capturing and returning all subprocess stdout.
-    module_names = run_python_forward_stderr_return_stdout(
+    module_names_str = run_python_forward_stderr_return_stdout(
         python_args=_PYTHON_ARGS)
 
     #FIXME: Actually parse "module_names" for bad entries here, please.
     # print(module_names)
+
+    # List of the fully-qualified names of all modules transitively imported by
+    # importing the @beartype decorator in an isolated Python interpreter.
+    module_names = module_names_str.splitlines()
+
+    # For each such name, assert this name is *NOT* that of a heavyweight
+    # (sub)module or package enumerated above.
+    #
+    # Note that this iteration could, of course, also be more efficiently
+    # implementable as a single composite regex match on the newline-delimited
+    # "module_names_str" string. However, doing so would entirely defeat the
+    # purpose of this iteration: to offer unambiguous and human-readable error
+    # messages in the common event of an importation violation.
+    for module_name in module_names:
+        assert re_search(_HEAVY_MODULE_NAME_REGEX, module_name) is None, (
+            f'@beartype.beartype improperly imports heavyweight module '
+            f'"{module_name}" from global scope.'
+        )
