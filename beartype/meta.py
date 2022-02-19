af566ed25b6fdc3dc89fdb5e2f8bf976f80e04e8
leycec <leycec@gmail.com>
2022-02-19 04:05:46 -0400

    **Beartype 0.10.2** started.

--- beartype/meta.py
+++ beartype/meta.py
@@ -200,7 +200,7 @@ if _PYTHON_VERSION_PARTS < PYTHON_VERSION_MIN_PARTS:
 # For further details, see http://semver.org.
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-VERSION = '0.10.1'
+VERSION = '0.10.2'
 '''
 Human-readable package version as a ``.``-delimited string.
 '''
