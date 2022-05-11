81699e009aade463966241fc8094fa36d9e164b4
leycec <leycec@gmail.com>
2022-05-11 03:08:43 -0400

    `beartype.claw` x 2.
    This commit is the next in a commit chain defining our new
    **all-at-once API** (i.e., an import hook enabling external callers to
    trivially decorate well-typed third-party packages and modules with
    runtime type-checking dynamically generated by the
    `beartype.beartype` decorator in a single line of code), en-route to
    resolving issue #43 kindly submitted by sentimental investor
    extraordinaire @ZeevoX. Specifically, this commit internally describes a
    possible avenue forward resolving all outstanding issues with import
    hook-based abstract syntax tree (AST) transformations performed by other
    competing packages (e.g., `typeguard`, `ideas`).
    
    Unrelatedly, this commit yet again disables previously enabled testing
    of pre-release builds of Python 3.11 under our GitHub Actions-based
    continuous integration (CI) workflow. Why? Because doing so foolishly
    attempts to compile NumPy from source, which unsurprisingly fails with
    catastrophic fireworks.
    
    (*Descriptively proactive proscription!*)

--- beartype/claw/_clawcore.py
+++ beartype/claw/_clawcore.py
@@ -10,6 +10,72 @@ This private submodule is *not* intended for importation by downstream callers.
 '''
 
 # ....................{ TODO                               }....................
+#FIXME: The New Five-Year Plan 2.0 is to avoid all interaction with the
+#higher-level "sys.meta_path" mechanism entirely. Why? Because pytest leverages
+#that some mechanism for its assertion rewriting. Do we care? *WE CARE,*
+#especially because there appears to be no sensible means of portably stacking
+#our own "MetaPathFinder" (...or whatever) on top of pytest's. Instead, we note
+#the existence of the much less commonly used (but ultimately significantly
+#safer) lower-level "sys.path_hooks" mechanism. Fascinatingly, AST transforms
+#can be implemented by leveraging either. For some reason, everyone *ONLY*
+#leverages the former to transform ASTs. Let's break that trend by instead
+#leveraging the latter to transform ASTs. Specifically:
+#* First, define a new private "_BeartypeSourceLoader(SourceFileLoader)" class
+#  strongly inspired by the *SECOND* example of this exemplary StackOverflow
+#  answer, which is probably the definitive statement on the subject:
+#  https://stackoverflow.com/a/43573798/2809027
+#  Note the use of the concrete "SourceFileLoader" superclass rather than the
+#  less concrete "FileLoader" superclass. Since both typeguard and ideas
+#  explicitly test for "SourceFileLoader" instances, it's almost certain that
+#  that's what we require as well.
+#  The disadvantage of this approach is that it fails to generalize to embedded
+#  Python modules (e.g., in frozen archives or zip files). Of course, *SO DOES*
+#  what "typeguard" and "ideas" are both doing and no one particularly seems to
+#  care there, right? This approach is thus still generally robust enough to
+#  suffice for a first pass.
+#  After getting this simplistic approach working, let's then fully invest in
+#  exhaustively testing that this approach successfully:
+#  * Directly decorates callables declared at:
+#    * Global scope in an on-disk top-level non-package module embedded in our
+#      test suite.
+#    * Class scope in the same module.
+#    * Closure scope in the same module.
+#  * Recursively decorates all callables declared by submodules of an on-disk
+#    top-level package.
+#  * Does *NOT* conflict with pytest's assertion rewriting mechanism. This will
+#    be non-trivial. Can we isolate another pytest process within the main
+#    currently running pytest process? O_o
+#* Next, generalize that class to support stacking. What? Okay, so the core
+#  issue with the prior approach is that it only works with standard Python
+#  modules defined as standard files in standard directories. This assumption
+#  breaks down for Python modules embedded within other files (e.g., as frozen
+#  archives or zip files). The key insight here is given by Iguananaut in this
+#  StackOverflow answer:
+#    https://stackoverflow.com/a/48671982/2809027
+#  This approach "...installs a special hook in sys.path_hooks that acts almost
+#  as a sort of middle-ware between the PathFinder in sys.meta_path, and the
+#  hooks in sys.path_hooks where, rather than just using the first hook that
+#  says 'I can handle this path!' it tries all matching hooks in order, until it
+#  finds one that actually returns a useful ModuleSpec from its find_spec
+#  method."
+#  Note that "hooks" in "sys.path_hooks" are actually *FACTORY FUNCTIONS*,
+#  typically defined by calling the FileFinder.path_hook() class method.
+#  We're unclear whether we want a full "ModuleSpec," however. It seems
+#  preferable to merely search for a working hook in "sys.path_hooks" that
+#  applies to the path. Additionally, if that hook defines a get_source() method
+#  *AND* that method returns a non-empty string (i.e., that is neither "None"
+#  *NOR* the empty string), then we want to munge that string with our AST
+#  transformation. The advantages of this approach are multitude:
+#  * This approach supports pytest, unlike standard "meta_path" approaches.
+#  * This approach supports embedded files, unlike the first approach above. In
+#    particular, note that the standard
+#    "zipimporter.zipimporter(_bootstrap_external._LoaderBasics)" class for
+#    loading Python modules from arbitrary zip files does *NOT* subclass any of
+#    the standard superclasses you might expect it to (e.g.,
+#    "importlib.machinery.SourceFileLoader"). Ergo, a simple inheritance check
+#    fails to suffice. Thankfully, that class *DOES* define a get_source()
+#    method resembling that of SourceFileLoader.get_source().
+
 #FIXME: Improve module docstring, please.
 
 # ....................{ IMPORTS                            }....................
