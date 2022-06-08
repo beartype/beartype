e1b6feb76b2bcb63eb5cf9b85fb35e7607375286
Talley Lambert <talley.lambert@gmail.com>
2022-06-08 16:20:11 -0400

    add __all__

--- beartype/math/__init__.py
+++ beartype/math/__init__.py
@@ -1,2 +1,3 @@
-from beartype.math._mathcls import TypeHint as TypeHint
-from beartype.math._mathcls import is_subtype as is_subtype
+from beartype.math._mathcls import TypeHint, is_subtype
+
+__all__ = ['TypeHint', 'is_subtype']
