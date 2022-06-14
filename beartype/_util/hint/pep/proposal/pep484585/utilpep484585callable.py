78e87e2c53ff20a508a8b9ee3fe8f8d450759261
leycec <leycec@gmail.com>
2022-06-14 03:54:33 -0400

    ``Callable[...]`` introspection x 4.
    This commit is the next in a commit chain defining a new internal
    private API for introspecting ``Callable[...]`` type hints at runtime,
    required as a mandatory dependency of pull request #136 kindly submitted
    by Harvard microscopist and all-around typing math guru @tlambert03
    (Talley Lambert). Specifically, this commit almost finalizes the draft
    implementation of our new high-level
    `get_hint_pep484585_callable_args()` getter, which currently only lacks
    support for `typing.ParamSpec[...]` and `typing.Concatenate[...]` child
    type hints. (*Actionable red redaction!*)

--- beartype/_util/hint/pep/proposal/pep484585/utilpep484585callable.py
+++ beartype/_util/hint/pep/proposal/pep484585/utilpep484585callable.py
@@ -87,14 +87,14 @@ def _die_unless_hint_pep484585_callable(
 
     Raises
     ----------
-    BeartypeDecorHintPepSignException
+    :exc:`exception_cls`
         If this hint is either:
 
         * PEP-compliant but *not* uniquely identifiable by a sign.
         * PEP-noncompliant.
         * *Not* a hint (i.e., neither PEP-compliant nor -noncompliant).
-    :exc:`exception_cls`
-        If this hint is *not* a callable type hint.
+        * *Not* a callable type hint (i.e., ``typing.Callable[...]`` or
+          ``collections.abc.Callable[...]``).
     '''
 
     # Avoid circular import dependencies.
@@ -176,9 +176,139 @@ def get_hint_pep484585_callable_args(
         If this hint is *not* a callable type hint.
     '''
 
+    # Avoid circular import dependencies.
+    from beartype._util.hint.pep.utilpepget import get_hint_pep_args
+
     # If this hint is *NOT* a callable type hint, raise an exception.
     _die_unless_hint_pep484585_callable(hint)
     # Else, this hint is a callable type hint.
 
-    #FIXME: Implement us up, please.
-    return ()
+    # Flattened tuple of the one or more child type hints subscripting this
+    # callable type hint. Presumably for space efficiency reasons, both PEP 484-
+    # *AND* 585-compliant callable type hints implicitly flatten the "__args__"
+    # dunder tuple from the original data structure subscripting those hints.
+    # CPython produces this flattened tuple as the concatenation of:
+    #
+    # * Either:
+    #   * If the first child type originally subscripting this hint was a list,
+    #     all items subscripting the nested list of zero or more parameter type
+    #     hints originally subscripting this hint as is: e.g.,
+    #         >>> Callable[[], bool]
+    #         (bool,)
+    #         >>> Callable[[int, str], bool]
+    #         (int, str, bool)
+    #     This includes a list containing only the empty tuple signifying a
+    #     callable accepting *NO* parameters, in which case that empty tuple is
+    #     preserved as is: e.g.,
+    #         >>> Callable[[()], bool]
+    #         ((), bool)
+    #   * Else, the first child type originally subscripting this hint as is. In
+    #     this case, that child type is required to be either:
+    #     * An ellipsis object (i.e., the "Ellipsis" builtin singleton): e.g.,
+    #         >>> Callable[..., bool]
+    #         (Ellipsis, bool)
+    #     * A PEP 612-compliant parameter specification (i.e.,
+    #       "typing.ParamSpec[...]" type hint): e.g.,
+    #         >>> Callable[ParamSpec('P'), bool]
+    #         (~P, bool)
+    #     * A PEP 612-compliant parameter concatenation (i.e.,
+    #       "typing.Concatenate[...]" type hint): e.g.,
+    #         >>> Callable[Concatenate[str, ParamSpec('P')], bool]
+    #         (typing.Concatenate[str, ~P], bool)
+    # * The return type hint originally subscripting this hint.
+    #
+    # Note that both PEP 484- *AND* 585-compliant callable type hints guarantee
+    # this tuple to contain at least one child type hint. Ergo, we avoid
+    # validating that constraint here: e.g.,
+    #     >>> from typing import Callable
+    #     >>> Callable[()]
+    #     TypeError: Callable must be used as Callable[[arg, ...], result].
+    #     >>> from collections.abc import Callable
+    #     >>> Callable[()]
+    #     TypeError: Callable must be used as Callable[[arg, ...], result].
+    hint_args = get_hint_pep_args(hint)
+
+    # Number of parameter type hints flattened into this tuple, calculated by
+    # excluding the trailing return type hint also flattened into this tuple.
+    #
+    # Note that by the above constraint, this number is guaranteed to be
+    # non-negative: e.g.,
+    #     >>> hint_args_len >= 0
+    #     True
+    hint_params_len = len(hint_args) - 1
+
+    # If this callable type hint was subscripted by *NO* parameter type hints,
+    # return the empty tuple for efficiency.
+    if hint_params_len == 0:
+        return ()
+    # Else, this callable type hint was subscripted by one or more parameter
+    # type hints.
+    #
+    # If this callable type hint was subscripted by two or more parameter type
+    # hints, this callable type hint *CANNOT* have been subscripted by a single
+    # "special" parameter type hint (e.g., ellipsis, parameter specification).
+    # By elimination, the only remaining category of parameter type hint is a
+    # nested list of two or more parameter type hints. In this case, return the
+    # tuple slice containing the parameter type hints omitting the trailing
+    # return type hint.
+    elif hint_params_len >= 2:
+        return hint_args[:-1]
+    # Else, this callable type hint was subscripted by exactly one parameter
+    # type hint... which could be either a nested list of one or more parameter
+    # type hints *OR* a "special" parameter type hint. To differentiate the
+    # former from the latter, we explicitly detect all possible instances of the
+    # latter and only fallback to the former after exhausting the latter.
+    else:
+        # Empty tuple singleton. Yes, we know exactly what you're thinking: "Why
+        # would anyone do this, @leycec? Why not just directly access the empty
+        # tuple singleton as ()?" Because Python insanely requires us to do this
+        # under Python >= 3.8 to detect empty tuples:
+        #     $ python3.7
+        #     >>> () is ()
+        #     True   # <-- yes, this is good
+        #
+        #     $ python3.8
+        #     >>> () is ()
+        #     SyntaxWarning: "is" with a literal. Did you mean "=="?  # <-- WUT
+        #     >>> _TUPLE_EMPTY = ()
+        #     >>> _TUPLE_EMPTY is _TUPLE_EMPTY
+        #     True  # <-- *FACEPALM*
+        #
+        # Note there's no point in globalizing this. Doing so would needlessly
+        # incur a globals lookup cost while saving no space or time.
+        _TUPLE_EMPTY = ()
+
+        # Single parameter type hint subscripting this callable type hint.
+        hint_param = hint_args[0]
+
+        # If this parameter type hint is either...
+        if (
+            # The empty tuple, reduce this unlikely (albeit possible) edge case
+            # to the empty tuple returned for the more common case of a callable
+            # type hint subscripted by an empty list. That is, reduce these two
+            # cases to the same empty tuple for simplicity:
+            #     >>> Callable[[], bool].__args__
+            #     (bool,)  # <------ this is good
+            #     >>> Callable[[()], bool].__args__
+            #     ((), bool,)  # <-- this is bad, so pretend this never happens
+            hint_param is _TUPLE_EMPTY or
+            # An ellipsis, return an ellipsis.
+            hint_param is ...
+        ):
+            return hint_param
+        # Else, this parameter type hint is neither the empty tuple *NOR* an
+        # ellipsis.
+
+        #FIXME: Handle "ParamSpec" and "Concatenate" here, please. The sanest
+        #way would be by calling get_hint_pep_sign_or_none() rather than crude
+        #isinstance() checks that will almost certainly break sometime. Don't
+        #bother making this conditional on a Python version. Just do it. Huzzah!
+
+        # Ergo, this parameter type hint is *NOT* be "special" and *MUST* thus
+        # be the single parameter type hint of a nested list. In this case,
+        # return the 1-tuple containing exactly this hint.
+        else:
+            return (hint_param,)
+
+    # Raise an exception, which should *NEVER* happen. But it will. Oh, it will.
+    raise exception_cls('This should never happen... but it did. Oh, Gods.')
