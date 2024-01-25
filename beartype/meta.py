be953ce0ce27467ab49e9c4927927108d29b5468
leycec <leycec@gmail.com>
2024-01-25 04:02:22 -0400

    **Beartype 0.17.1** started.

--- beartype/meta.py
+++ beartype/meta.py
@@ -209,7 +209,7 @@ if _PYTHON_VERSION_PARTS < PYTHON_VERSION_MIN_PARTS:
 # For further details, see http://semver.org.
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-VERSION = '0.17.0'
+VERSION = '0.17.1'
 '''
 Human-readable package version as a ``.``-delimited string.
 '''
