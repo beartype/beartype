576529e7830723924fac5a15e140bc63f47b0645
leycec <leycec@gmail.com>
2024-06-29 02:55:39 -0400

    **Beartype 0.19.0 Release Candidate 1** started.

--- beartype/meta.py
+++ beartype/meta.py
@@ -137,7 +137,7 @@ metadata).
 '''
 
 # ....................{ METADATA ~ version                 }....................
-VERSION = '0.19.0rc0'
+VERSION = '0.19.0rc1'
 '''
 Human-readable package version as a ``.``-delimited string.
 '''
