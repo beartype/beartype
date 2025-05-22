221fe7ff24132eb9d16bf59df636bc82e007d5e9
leycec <leycec@gmail.com>
2025-05-22 01:01:48 -0400

    **Beartype 0.21.1** started.

--- beartype/meta.py
+++ beartype/meta.py
@@ -141,7 +141,7 @@ metadata).
 '''
 
 # ....................{ METADATA ~ version                 }....................
-VERSION = '0.21.0'
+VERSION = '0.21.1'
 '''
 Human-readable package version as a ``.``-delimited string.
 '''
