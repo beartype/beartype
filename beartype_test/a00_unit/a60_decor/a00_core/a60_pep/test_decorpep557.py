70ae5f99a4f4d5b43da7fc93b870d4e915c5c412
leycec <leycec@gmail.com>
2026-03-04 02:47:59 -0400

    PEP 557 + PEP 484 forward references.
    This commit generalizes @beartype's support for PEP 557-compliant
    **dataclasses** (i.e., `@dataclasses.dataclass`-decorated types)
    defining one or more fields annotated by PEP 484-compliant stringified
    forward references in either absolute or relative syntactic form (e.g.,
    `muh_field: Optional['MuhUndefinedType'] = None`), resolving
    long-standing feature request #527 kindly submitted a lifetime ago by
    stalwart synthwave QA hero @ddorian. Specifically, this commit
    exhaustively tests that this is the case. Apparently, this is the case.
    Exhaustion: *it's why we're still here.*
    (*Contemporaneous temporariness: contemplate templates!*)

--- beartype_test/a00_unit/a60_decor/a00_core/a60_pep/test_decorpep557.py
+++ beartype_test/a00_unit/a60_decor/a00_core/a60_pep/test_decorpep557.py
@@ -34,10 +34,6 @@ def test_decor_pep557() -> None:
         BeartypeCallHintPep557FieldViolation,
         BeartypeCallHintParamViolation,
     )
-    from beartype.typing import (
-        ClassVar,
-        Optional,
-    )
     from dataclasses import (
         FrozenInstanceError,
         InitVar,
@@ -45,6 +41,10 @@ def test_decor_pep557() -> None:
         field,
     )
     from pytest import raises
+    from typing import (
+        ClassVar,
+        Optional,
+    )
 
     # ..................{ LOCALS                             }..................
     # Beartype decorator type-checking *ALL* PEP 557-compliant dataclass fields.
@@ -74,7 +74,7 @@ def test_decor_pep557() -> None:
             Arbitrary dataclass type-checked by :func:`beartype.beartype`.
             '''
 
-            # ..................{ PARAMETERS ~ mandatory     }..................
+            # ..................{ MANDATORY ~ pep : 557      }..................
             but_for_such_faith: InitVar[str]
             '''
             Mandatory non-field parameter passed by the :func:`dataclass`
@@ -87,27 +87,48 @@ def test_decor_pep557() -> None:
             parameters. *sigh*
             '''
 
-            # ..................{ PARAMETERS ~ optional      }..................
+            # ..................{ OPTIONAL ~ pep : 484       }..................
             that_man_may_be: Optional[str] = None
             '''
-            Optional field passed by the :func:`.dataclass` decorator to
-            the implicit :meth:`__init__` method synthesized for this dataclass.
+            Optional field annotated by an arbitrary PEP-noncompliant type,
+            passed by the :func:`.dataclass` decorator to the implicit
+            :meth:`__init__` method synthesized for this dataclass.
             '''
 
 
-            thou_hast_a_voice: int = field(default=0xBABE, init=False)
+            glowed_through: Optional['TheMufflingDark'] = None
             '''
-            Optional uninitialized field *not* passed by the :func:`.dataclass`
+            Optional field annotated by a :pep:`484`-compliant stringified
+            relative forward reference type hint intentionally referring to a
+            type that has yet to be defined, passed by the :func:`.dataclass`
             decorator to the implicit :meth:`__init__` method synthesized for
             this dataclass.
             '''
 
 
+            sweet_shaped_lightnings: Optional[
+                'beartype_test.a00_unit.data.data_type.Class'] = None
+            '''
+            Optional field annotated by a :pep:`484`-compliant stringified
+            absolute forward reference type hint intentionally referring to an
+            external type, passed by the :func:`.dataclass` decorator to the
+            implicit :meth:`__init__` method synthesized for this dataclass.
+            '''
+
+            # ..................{ NON-INIT ~ pep : 526       }..................
             with_nature_reconciled: ClassVar[bool] = True
             '''
-            Optional class variable ignored by the :func:`.dataclass` decorator
-            and thus *not* passed to the implicit :meth:`__init__` method
-            synthesized for this dataclass.
+            Class variable ignored by the :func:`.dataclass` decorator and thus
+            *not* passed to the implicit :meth:`__init__` method synthesized by
+            this dataclass.
+            '''
+
+            # ..................{ NON-INIT ~ pep : 557       }..................
+            thou_hast_a_voice: int = field(default=0xBABE, init=False)
+            '''
+            Uninitialized field *not* passed by the :func:`.dataclass` decorator
+            to the implicit :meth:`__init__` method synthesized by this
+            dataclass.
             '''
 
             # ..................{ INITIALIZERS               }..................
@@ -121,16 +142,16 @@ def test_decor_pep557() -> None:
                 '''
 
                 # If the caller failed to pass this optional field to the
-                # __init__() constructor, default this field to this mandatory
-                # field required to be passed by the caller.
+                # __init__() constructor, default this field to this other
+                # mandatory field required to be passed by the caller.
                 #
-                # Note that this field *CANNOT* be safely assigned with a simple
-                # assignment statement: e.g.,
+                # Note that this optional field *CANNOT* be safely assigned with
+                # a simple assignment statement: e.g.,
                 #     self.that_man_may_be = but_for_such_faith
                 #
                 # Doing so suffices for the general case but fails for frozen
                 # dataclasses, which enforce immutability even within this
-                # __post_init__() initialization method, which arguably
+                # __post_init__() initialization method. Inarguably, this
                 # constitutes a long-standing bug that CPython devs will
                 # probably *NEVER* resolve: e.g.,
                 #     dataclasses.FrozenInstanceError: cannot assign to field
@@ -139,7 +160,7 @@ def test_decor_pep557() -> None:
                 # Instead, we assign this field by circumventing the
                 # __setattr__() dunder method defined for frozen dataclasses
                 # entirely in favour of a permissive alternative guaranteed to
-                # exist: the object.__setattr__() dunder method. We sigh.
+                # exist: the object.__setattr__() dunder method. We sigh. *sigh*
                 if self.that_man_may_be is None:
                     object.__setattr__(
                         self, 'that_man_may_be', but_for_such_faith)
@@ -157,18 +178,48 @@ def test_decor_pep557() -> None:
         # True only if this dataclass is slotted, defaulting to false.
         IS_DATACLASS_SLOTTED = dataclass_kwargs.get('slots', False)
 
-        # ..................{ ASSERTS ~ fields               }..................
+        # ..................{ ASSERTS ~ fields : init        }..................
         # Assert that this dataclass initializes these fields to have the
         # expected initial values.
         assert great_mountain.that_man_may_be == (
             'So solemn, so serene, that man may be,')
+        assert great_mountain.glowed_through is None
+        assert great_mountain.sweet_shaped_lightnings is None
         assert great_mountain.thou_hast_a_voice == 0xBABE
 
+        # ..................{ ASSERTS ~ forward : define     }..................
+        # Defer the importation of this type until *AFTER* accessing the
+        # "SoSolemnSoSerene.sweet_shaped_lightnings" optional field annotated by
+        # a PEP 484-compliant stringified absolute forward reference type hint
+        # referring to this type.
+        from beartype_test.a00_unit.data.data_type import Class
+
+        class TheMufflingDark(object):
+            '''
+            Arbitrary class intentionally defined *after* accessing the
+            :attr:`.SoSolemnSoSerene.glowed_through` optional field annotated by
+            a :pep:`484`-compliant stringified relative forward reference type
+            hint referring to this class.
+            '''
+
+            pass
+
+        # Instance of these types imported and/or defined above.
+        and_wrought_upon = TheMufflingDark()
+        from_the_nadir_deep = Class()
+
+        # ..................{ ASSERTS ~ fields : set         }..................
         # Assert that attempting to set these fields to new values violating the
         # hints annotating these fields raises the expected exception.
         with raises(BeartypeCallHintPep557FieldViolation):
             great_mountain.that_man_may_be = (
                 b'Somewhere between the throne, and where I sit')
+        with raises(BeartypeCallHintPep557FieldViolation):
+            great_mountain.glowed_through = (
+                "Glow'd through, and wrought upon the muffling dark")
+        with raises(BeartypeCallHintPep557FieldViolation):
+            great_mountain.sweet_shaped_lightnings = (
+                'Sweet-shaped lightnings from the nadir deep')
         with raises(BeartypeCallHintPep557FieldViolation):
             great_mountain.thou_hast_a_voice = (
                 'Here on this spot of earth. Search, Thea, search!')
@@ -182,6 +233,10 @@ def test_decor_pep557() -> None:
                 great_mountain.that_man_may_be = (
                     'With backward footing through the shade a space:')
             with raises(FrozenInstanceError):
+                great_mountain.glowed_through = and_wrought_upon
+            with raises(FrozenInstanceError):
+                great_mountain.sweet_shaped_lightnings = from_the_nadir_deep
+            with raises(FrozenInstanceError):
                 great_mountain.thou_hast_a_voice = len(
                     "He follow'd, and she turn'd to lead the way")
         # Else, this dataclass is *NOT* frozen. In this case...
@@ -192,10 +247,24 @@ def test_decor_pep557() -> None:
                 'Through aged boughs, that yielded like the mist')
             assert great_mountain.that_man_may_be == (
                 'Through aged boughs, that yielded like the mist')
+
+            great_mountain.glowed_through = and_wrought_upon
+            assert great_mountain.glowed_through is and_wrought_upon
+
+            great_mountain.sweet_shaped_lightnings = from_the_nadir_deep
+            assert great_mountain.sweet_shaped_lightnings is from_the_nadir_deep
+
             great_mountain.thou_hast_a_voice = len(
                 'Which eagles cleave upmounting from their nest.')
             assert great_mountain.thou_hast_a_voice == len(
                 'Which eagles cleave upmounting from their nest.')
+
+        # ..................{ ASSERTS ~ forward : delete     }..................
+        # Delete the types imported and/or defined above referred to by forward
+        # references defined above to properly validate forward reference
+        # resolution on the next loop iteration. Safety is our name! *gags*
+        del Class
+        del TheMufflingDark
 
         # ..................{ ASSERTS ~ InitVar              }..................
         # Assert that this dataclass does *NOT* expose initialization-only
